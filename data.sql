e611f2c8f9959c7e95b9026b4544ee74e50970b3
lisandroseia <lisandroseia11@gmail.com>
2022-03-22 20:11:36 -0300

    Add species, querys and transactions

--- data.sql
+++ data.sql
@@ -42,3 +42,21 @@ INSERT INTO
     )
 VALUES
     ('Devimon', '2017-05-12', 5, true, 11);
+
+/* day 2 */
+INSERT INTO
+    animals (
+        name,
+        date_of_birth,
+        escape_attempts,
+        neutered,
+        weight_kg
+    )
+VALUES
+    ('Charmander', '2020-02-08', 0, false, -11),
+    ('Plantmon', '2021-11-15', 2, true, -5.7),
+    ('Squirtle', '1993-07-02', 3, false, -12.13),
+    ('Angemon', '2005-05-12', 1, true, -45),
+    ('Boarmon', '2005-05-07', 7, true, 20.4),
+    ('Blossom', '1998-10-13', 3, true, 17),
+    ('Ditto', '2022-05-14', 4, true, 22);
